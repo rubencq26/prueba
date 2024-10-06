b21a2137e5f7a16dbffdb8c7ae156b76dfb000b8
Ruben Conde <rubconqui@gmail.com>
2024-10-06 15:05:19 +0200

    Añadimos comentario

--- pruebaProyectt/src/App.java
+++ pruebaProyectt/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         int x = 7;
         System.out.println(x);
+        //Esto es un comentario para subirlo namas
 
     }
 }
